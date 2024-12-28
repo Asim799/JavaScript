7678d9b0be07282387cea9581d8be4cc4db64fff
Asim Churihar <asimchurihar1@gmail.com>
2024-12-28 20:55:53 +0530

    added new file

--- 01_String.js
+++ 01_String.js
@@ -4,9 +4,21 @@ let repoCount = 50;
 
 console.log(`Hello my name is ${name} and my RepoCount is ${repoCount}.`);
 
-const myName = new String("AsimC");
+const myName = new String("AsimChurihar");
 console.log(myName[0]);
 console.log(myName.length);
 console.log(myName.toUpperCase());
 console.log(myName.charAt(4));
 console.log(myName.indexOf("s"));
+const newString = myName.substring(0, 4);
+console.log(newString);
+const anotherString = myName.slice(-8, -4);
+console.log(anotherString);
+
+const newString2 = "     Asim    ";
+console.log(newString2);
+console.log(newString2.trim());
+
+const URL = "https://www.google.com/asim%20churihar";
+
+console.log(URL.replace("%20", "-"));
