4204b1c36a4108ce3ac11c03be0a02c29a31ee03
Asim Churihar <asimchurihar1@gmail.com>
2024-12-28 20:34:46 +0530

    added new file

---
+++ 01_String.js
@@ -0,0 +1,12 @@
+const name = "Asim Churihar";
+let repoCount = 50;
+//console.log(name + repoCount);
+
+console.log(`Hello my name is ${name} and my RepoCount is ${repoCount}.`);
+
+const myName = new String("AsimC");
+console.log(myName[0]);
+console.log(myName.length);
+console.log(myName.toUpperCase());
+console.log(myName.charAt(4));
+console.log(myName.indexOf("s"));
