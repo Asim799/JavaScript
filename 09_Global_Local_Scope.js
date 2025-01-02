336393fb921c009885ecf166c141f6548d3f9f00
Asim799 <mohammadasim799@gmai.com>
2025-01-02 12:14:22 +0530

    Made some changes

--- 09_Global_Local_Scope.js
+++ 09_Global_Local_Scope.js
@@ -14,3 +14,29 @@ const b = 60;// Now this is global scope
 console.log(a);
 console.log(b);
 console.log(c);
+
+//local and global scope in functions:
+function one(){
+  const username = "Asim";
+  function two(){
+    const website = "codeByte"
+    console.log(username);
+  }
+ // console.log(website); local scope
+  two()
+}
+//console.log(username); local scope
+one()
+
+
+//Global and local scope in if-else condition:
+if(true){
+  const username = "Asim"
+  if(true){
+    const website = "codeByte"
+    console.log(username,website);
+  }
+  //console.log(username,website);
+}
+
+//console.log(username, website);
