f97b6545e9104c7bdb1d07bb797cb8d52818ddc2
Asim Churihar <asimchurihar1@gmail.com>
2025-01-04 21:06:59 +0530

    made some changes

--- 13_ifElse.js
+++ 13_ifElse.js
@@ -51,3 +51,18 @@ if (Object.keys(check) == 0) {
   //for an empty object
   console.log("Executed");
 }
+
+// Nullish Coalescing Operator: (??) null and undefined
+// let val1 = 5 ?? 10;
+// let val1 = null ?? 20; // val1 = 20;
+// let val1 = undefined ?? 30; //val1 = 30;
+let val1 = null ?? 10 ?? 30; // first value i.e 10 will be stored in val1
+
+console.log(val1);
+
+//Terniary Operator:
+
+// condition ? true:false
+
+const coursePrice = 1000;
+coursePrice >= 999 ? console.log("Wow Nice!") : console.log("Not Satisfied");
