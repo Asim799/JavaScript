1ff18b6cb6e7b3a0ba4cfbaa1f1b5f561489ae8b
Asim Churihar <asimchurihar1@gmail.com>
2025-01-04 21:00:03 +0530

    made some changes

--- 13_ifElse.js
+++ 13_ifElse.js
@@ -18,3 +18,36 @@ if (userLoggedInViaGoogle || userLoggedInViaMobile) {
   //use of OR operator with IF condition
   console.log(" User Logged in successfully!");
 }
+
+//if there is no comparison:
+
+const userEmail = "asim@one.com";
+
+if (userEmail) {
+  console.log("User has an email");
+}
+
+//Falsy Values: false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+// const check = "";
+// const check = false;
+// const check = 0;
+// const check = -0;
+// const check = null;
+// const check = undefined;
+
+//Truthy values: "false", "0", [], {}, function(){}
+
+// const check = "false";
+// const check = "0";
+// const check = [];
+const check = {};
+
+// if (check.length == 0) {         //for empty array
+//   console.log("Executed");
+// }
+
+if (Object.keys(check) == 0) {
+  //for an empty object
+  console.log("Executed");
+}
