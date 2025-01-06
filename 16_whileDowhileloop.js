3dd8dc17d0d8b7f2811df73d9e78f6ef63395687
Asim799 <mohammadasim799@gmai.com>
2025-01-06 09:46:23 +0530

    New File Added

---
+++ 16_whileDowhileloop.js
@@ -0,0 +1,14 @@
+//while loop:if condition is true then the statement will get executed
+let count = 0;
+while(count<=10){
+  console.log(`The count is ${count}`);
+  count++
+}
+
+//do while loop: atleast one statement is executed
+let num = 1
+  do{
+    console.log(`I am the number ${num}`);
+  num++
+  }
+  while(num<10)
